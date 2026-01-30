781ba619618b357fb4c685a780b83b68365b3661
OshadaX <navindraoshada@gmail.com>
2026-01-30 19:28:51 +0530

    Update employee routes

--- backend/src/routes/member3-oshada/employee.routes.js
+++ backend/src/routes/member3-oshada/employee.routes.js
@@ -2,12 +2,14 @@ const express = require('express');
 const router = express.Router();
 const {
     getEmployees,
+    loginEmployee,
     createEmployee,
     updateEmployee,
     deleteEmployee
 } = require('../../controllers/member3-oshada/employeeController');
 
 router.get('/', getEmployees);
+router.post('/login', loginEmployee);
 router.post('/', createEmployee);
 router.put('/:id', updateEmployee);
 router.delete('/:id', deleteEmployee);
